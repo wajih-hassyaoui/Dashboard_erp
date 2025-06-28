a05b2a0a65b79e4d23cd04eb006244c7597ae4ca
wajih <wajih.hassyaoui@outlook.com>
2025-06-28 13:54:27 +0100

    add a configuration in backend

---
+++ Backend/src/main/java/com/dashboard_erp/backend/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.dashboard_erp.backend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT
+        ));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setMaxAge(3600L);
+    source.registerCorsConfiguration("/**", config);
+
+    FilterRegistrationBean bean= new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(-102);
+    return bean;
+}}
